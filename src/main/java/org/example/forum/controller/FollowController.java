016bda9aeb75535b1ff25713944362b07cad6bc0
yungson <74695724+yungson@users.noreply.github.com>
2022-06-02 16:27:30 -0700

    add followees and followers list page

--- src/main/java/org/example/forum/controller/FollowController.java
+++ src/main/java/org/example/forum/controller/FollowController.java
@@ -1,14 +1,22 @@
 package org.example.forum.controller;
 
+import org.example.forum.entity.Page;
 import org.example.forum.entity.User;
 import org.example.forum.service.FollowService;
+import org.example.forum.service.UserService;
+import org.example.forum.util.ForumConstant;
 import org.example.forum.util.ForumUtil;
 import org.example.forum.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class FollowController {
@@ -18,6 +26,9 @@ public class FollowController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/follow", method = RequestMethod.POST)
     @ResponseBody
@@ -33,5 +44,54 @@ public class FollowController {
         User user = hostHolder.getUser();
         followService.unfollow(user.getId(), entityType, entityId);
         return ForumUtil.getJSONString(0, "已取消关注");
+    }
+
+    @RequestMapping(path = "/followees/{userId}", method = RequestMethod.GET)
+    public String getFollowees(@PathVariable("userId") int userId, Page page, Model model){
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("User does not exist");
+        }
+        model.addAttribute("user", user);
+        page.setLimit(5);
+        page.setPath("/followees/"+userId);
+        page.setRows((int) followService.findFolloweeCount(userId, ForumConstant.ENTITY_TYPE_USER));
+        List<Map<String, Object>> userList = followService.findFollowees(userId, page.getOffset(), page.getLimit());
+        if (userList!=null) {
+            for( Map<String, Object> map: userList){
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+        return "/site/followee";
+    }
+
+    private boolean hasFollowed(int userId){
+        if (hostHolder.getUser() == null){
+            return false;
+        }
+        return followService.findHasFollowed(hostHolder.getUser().getId(), ForumConstant.ENTITY_TYPE_USER, userId);
+    }
+
+    @RequestMapping(path = "/followers/{userId}", method = RequestMethod.GET)
+    public String getFollowers(@PathVariable("userId") int userId, Page page, Model model){
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("User does not exist");
+        }
+        model.addAttribute("user", user);
+        page.setLimit(5);
+        page.setPath("/followers/"+userId);
+        page.setRows((int) followService.findFollowerCount(ForumConstant.ENTITY_TYPE_USER, userId));
+        List<Map<String, Object>> userList = followService.findFollowers(userId, page.getOffset(), page.getLimit());
+        if (userList!=null) {
+            for( Map<String, Object> map: userList){
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+        return "/site/follower";
     }
 }
--- src/main/java/org/example/forum/service/FollowService.java
+++ src/main/java/org/example/forum/service/FollowService.java
@@ -1,5 +1,7 @@
 package org.example.forum.service;
 
+import org.example.forum.entity.User;
+import org.example.forum.util.ForumConstant;
 import org.example.forum.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -8,11 +10,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
 public class FollowService {
 
     @Autowired
     private RedisTemplate redisTemplate;
+    @Autowired
+    private UserService userService;
 
     public void follow(int userId, int entityType, int entityId) {
         redisTemplate.execute(new SessionCallback() {
@@ -61,6 +67,45 @@ public class FollowService {
         String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
         System.out.println("userId = " + userId + ", entityType = " + entityType + ", entityId = " + entityId);
         return redisTemplate.opsForZSet().score(followeeKey, entityId) != null;
+    }
+
+    // Look up for the followee of somebody
+    public List<Map<String, Object>> findFollowees(int userId, int offset, int limit) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, ForumConstant.ENTITY_TYPE_USER);
+        Set<Integer> targetIds = redisTemplate.opsForZSet().reverseRange(followeeKey, offset, offset+limit-1); // for redis, end is included
+        if (targetIds == null) {
+            return null;
+        }
+        List<Map<String, Object>> list = new ArrayList<>();
+        for(Integer targetId: targetIds){
+            Map<String, Object> map = new HashMap<>();
+            User user = userService.findUserById(targetId);
+            map.put("user", user);
+            Double score = redisTemplate.opsForZSet().score(followeeKey, targetId);
+            map.put("followTime", new Date(score.longValue()));
+            list.add(map);
+        }
+        return list;
+    }
+
+    // look up for the fans of somebody
+    public List<Map<String, Object>> findFollowers(int userId, int offset, int limit){
+        String followerKey = RedisKeyUtil.getFollowerKey(ForumConstant.ENTITY_TYPE_USER, userId);
+        // redisTemplate 返回的Set<Integer> 是redis自己实现的， 是有序的。java自带的Set是无序的
+        Set<Integer> fromIds = redisTemplate.opsForZSet().reverseRange(followerKey, offset, offset+limit-1);
+        if ( fromIds == null) {
+            return null;
+        }
+        List<Map<String, Object>> list = new ArrayList<>();
+        for(Integer fromId: fromIds){
+            Map<String, Object> map = new HashMap<>();
+            User user = userService.findUserById(fromId);
+            map.put("user", user);
+            Double score = redisTemplate.opsForZSet().score(followerKey, fromId);
+            map.put("followTime", new Date(score.longValue()));
+            list.add(map);
+        }
+        return list;
     }
 
 }
