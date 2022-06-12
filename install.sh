6135f347374c288d71d6b8d5bded4030f98445a7
yungson <yungson>
2022-06-12 20:08:19 +0000

    config elastic search only to accept connections from inside docker network

--- install.sh
+++ install.sh
@@ -82,10 +82,10 @@ export data_tomcat=${deploy_root}/tomcat
 export data_upload_dir=${data_system}/upload
 export wk_image_storage=${data_system}/wkhtmltopdf
 export wk_image_command="docker run -it --rm -v ${wk_image_storage}:${wk_image_storage} --entrypoint /wkhtmltox/bin/wkhtmltoimage osones/wkhtmltopdf"
-export datasource_password=d50960f067142c59cc3bdac61b87759f
-export qqmail_authorization_code=zujhxfscwcygdiag
-export qiniu_access_key=AYNuEVq1TpUC9lTwbZYLAnR6nxCLdN5-_uidboBM
-export qiniu_access_secret=KUygoc1blB8yY3eGwegSmGbZr5SYcCUsdya8oSiQ
+export datasource_password=${datasource_password}
+export qqmail_authorization_code=${qqmail_authorization_code}
+export qiniu_access_key=${qiniu_access_key}
+export qiniu_access_secret=${qiniu_access_secret}
 
 
 mkdir -p $data_mysql $data_redis $data_elasticsearch $data_zookeeper $data_kafka  $data_tomcat/webapps $data_upload_dir $wk_image_storage
